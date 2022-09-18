0733afac6ab8bad467e9f76b584720b8e5da296b
Sef99 <enriquecaballero01@hotmail.com>
2022-09-18 01:42:14 -0400

    add eliminar horarios atencion

--- src/app/horarios/horarios.component.ts
+++ src/app/horarios/horarios.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ConfirmationDialog } from '../shared/confirm-delete.component';
 import { HorarioAtencion } from './horario.model';
 import { HorarioAtencionService } from './horarios.service';
 
@@ -23,7 +27,7 @@ export class HorariosComponent implements OnInit {
     this.dataSource.sort = this.sort;
   }
 
-  constructor(private horarioAtencionService: HorarioAtencionService) {}
+  constructor(private horarioAtencionService: HorarioAtencionService, private route: ActivatedRoute, private router: Router,private snackBar: MatSnackBar, private dialog: MatDialog,) {}
 
   ngOnInit() {
     this.get();
@@ -31,6 +35,30 @@ export class HorariosComponent implements OnInit {
 
   get(): void {
     this.horarioAtencionService.getHorarios().subscribe(data=> this.dataSource.data = data.lista);
+  }
+
+  openDialog(id: number) {
+    const dialogRef = this.dialog.open(ConfirmationDialog,{
+      data:{
+        message: 'Are you sure want to delete?',
+        buttonText: {
+          ok: 'Save',
+          cancel: 'No'
+        }
+      }
+    });
+
+    dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+      if (confirmed) {
+        this.horarioAtencionService.deleteHorario(id).subscribe(horario => console.log(horario));
+        const a = document.createElement('a');
+        a.click();
+        a.remove();
+        this.snackBar.open('Deleted', 'Close', {
+          duration: 2000,
+        });
+      }
+    });
   }
 
 }
